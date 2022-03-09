91c3c91c75da2a23ba93b1a594ccb5d2934745e9
rpmzamer <mayankaryarpm@gmail.com>
2022-03-09 21:59:01 +0530

    Pattern 10 Efficient Approach

--- Pattern/Pattern10.java
+++ Pattern/Pattern10.java
@@ -1,50 +1,42 @@
 import java.util.*;
 
-public class Pattern10{
+public class Pattern10 {
 
-public static void main(String[] args) {
-    Scanner scn = new Scanner(System.in);
-    int n=scn.nextInt();
-    int row=1,inrspac=1,nspace=n/2,i;
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n=scn.nextInt();
+        int row=1,nstar=1,nspace=n/2;
         while(row<=n)
         {
-            for(i=1;i<=nspace;i++)
+            for(int i=1;i<=nspace;i++)
             {
                 System.out.print("\t");
             }
-            if(row==1 || row==n)
+            for(int i=1;i<=nstar;i++)
             {
-               System.out.print("*\t"); 
-            }
-            else
-            {
-                System.out.print("*\t");
-                for(i=1;i<=inrspac;i++)
-                {
-                    System.out.print("\t");
-                }
-                System.out.print("*\t");
+                if(i==1||i==nstar)
+               {
+                   System.out.print("*\t");
+               }
+               else
+               {
+                   System.out.print("\t");  
+               }
+
             }
             System.out.println();
             if(row<=n/2)
             {
                 nspace-=1;
-                if(row!=1 && row!=n)
-                {
-                    inrspac+=2;
-                }
+                nstar+=2;
             }
             else
             {
                 nspace+=1;
-                if(row!=1 && row!=n)
-                {
-                    inrspac-=2;
-                }
-                
+                nstar-=2;
             }
             
             row++;
         }
- }
+    }
 }
