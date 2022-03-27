e3870164f9384fabcc4c6b7faa3f89e30aa287e0
rpmzamer <mayankaryarpm@gmail.com>
2022-03-27 20:09:04 +0530

    Guess Number Higher or Number Leetcode

---
+++ Fuction and Arrays/GuessNumberHigherOrNumber_Leetcode.java
@@ -0,0 +1,25 @@
+public class GuessNumberHigherOrNumber_Leetcode {
+    public int guessNumber(int n)
+    {
+        int low=1,high=n,mid;
+        while(low<=high)
+        {
+            mid=low+(high-low)/2;
+            if(guess(mid)==-1)
+            {
+                high=mid-1;
+               
+            }
+            else if(guess(mid)==1)
+            {
+                low=mid+1;
+            }
+            else
+            {
+                return mid;
+            } 
+        }
+        return 0;
+    }
+    
+}
