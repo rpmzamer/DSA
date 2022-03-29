4459cf45199cae6c99a90903841569fff2036e31
rpmzamer <mayankaryarpm@gmail.com>
2022-03-29 19:55:52 +0530

    2D Arrays Demo using functions

--- 2D Arrays/TwoDArraysDemo.java
+++ 2D Arrays/TwoDArraysDemo.java
@@ -2,26 +2,37 @@ import java.util.*;
   
   public class TwoDArraysDemo{
   
+    public static void input(int [][]mat1,Scanner scn)
+    {
+      for(int r=0;r<mat1.length;r++)
+    {
+      for(int c=0;c<mat1[0].length;c++)
+      {
+        mat1[r][c]=scn.nextInt();
+      }
+    }
+
+    }
+
+    public static void display(int [][]mat2)
+    {
+       for(int r=0;r<mat2.length;r++)
+    {
+      for(int c=0;c<mat2[0].length;c++)
+      {
+        System.out.print(mat2[r][c]+" ");
+      }
+      System.out.println();
+    }
+    }
+  
   public static void main(String[] args) {
     Scanner scn= new Scanner(System.in);
     int nr = scn.nextInt();
     int nc = scn.nextInt();
     int [][]mat = new int[nr][nc];
-    for(int r=0;r<mat.length;r++)
-    {
-      for(int c=0;c<mat[0].length;c++)
-      {
-        mat[r][c]=scn.nextInt();
-      }
-    }
     
-    for(int r=0;r<mat.length;r++)
-    {
-      for(int c=0;c<mat[0].length;c++)
-      {
-        System.out.print(mat[r][c]+" ");
-      }
-      System.out.println();
-    }
+    input(mat,scn);
+    display(mat);   
    }
   }
