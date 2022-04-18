826972c20c59988a4a5cd74dede40d4755fe235f
rpmzamer <mayankaryarpm@gmail.com>
2022-04-18 23:16:02 +0530

    Remove Prime

---
+++ String Dynamic and Dynamic Array/RemovePrime.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class RemovePrime{
+	public static int isPrime(int n)
+	{
+		int count=0;
+		for(int div=2;div*div<=n;div++)
+		{
+			if(n%div==0)
+			{
+				return -1;
+			}
+		}
+		return 0;
+	}
+	public static void solution(ArrayList<Integer> al)
+	{
+		for(int i=al.size()-1;i>-1;i--)
+		{
+			if(isPrime(al.get(i))!=-1)
+			{
+				al.remove(i);
+			}
+		}
+	}
+	public static void main(String[] args) 
+	{
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		ArrayList<Integer> al = new ArrayList<>();
+		for(int i = 0 ; i < n; i++)
+		{
+			al.add(scn.nextInt());
+		}
+		solution(al);
+		System.out.println(al);
+	}
+}
