5f6f9f4089604ca5b31a3409c4ed9530bccbf7dd
rpmzamer <mayankaryarpm@gmail.com>
2022-06-04 08:43:08 +0530

    2231. Largest Number After Digit Swaps by Parity

---
+++ String Dynamic and Dynamic Array/LeetCode/2231.LargestNumberAfterDigitSwapsbyParity.java
@@ -0,0 +1,61 @@
+class Solution {
+    public int largestInteger(int num) {
+        StringBuilder sb=new StringBuilder(num+"");
+        for(int i=0;i<sb.length();i++)
+        {
+            int maxIdx=i;
+            if(isEven(sb.charAt(i)))
+            {
+                maxIdx=findNextMaxEven(sb,i);
+            }
+            else
+            {
+                maxIdx=findNextMaxOdd(sb,i);
+            }
+            if(maxIdx!=i)
+            {
+                char temp=sb.charAt(i);
+                char temp2=sb.charAt(maxIdx);
+                sb.setCharAt(i,temp2);
+                sb.setCharAt(maxIdx,temp);
+            }
+         }
+        return Integer.parseInt(sb.toString());
+    }      
+    
+    public boolean isEven(char a)
+    {
+        if((a%2)==0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public int findNextMaxEven(StringBuilder sb,int idx)
+    {
+        int max=idx;
+        for(int j=idx+1;j<sb.length();j++)
+        {
+            if(sb.charAt(j)>sb.charAt(max) && isEven(sb.charAt(j)))
+            {
+                max=j;
+            }
+        }
+        return max;
+    }
+     public int findNextMaxOdd(StringBuilder sb,int idx)
+    {
+        int max=idx;
+        for(int j=idx+1;j<sb.length();j++)
+        {
+            if(sb.charAt(j)>sb.charAt(max) && !(isEven(sb.charAt(j))))
+            {
+                max=j;
+            }
+        }
+        return max;
+    }
+}
