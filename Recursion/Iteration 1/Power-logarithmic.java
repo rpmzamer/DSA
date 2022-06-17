6c46413d57db58a0acf4788d7ba92d04f9c0b9f9
rpmzamer <mayankaryarpm@gmail.com>
2022-06-17 23:05:13 +0530

    Power Logarithmic

---
+++ Recursion/Iteration 1/Power-logarithmic.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        
+    }
+
+    public static int power(int x, int n){
+        return 0;
+    }
+
+}
